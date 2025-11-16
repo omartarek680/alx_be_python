7b5ea0d44a30f51c4c91597c3cbfb159c8bf9e40
omartarek680 <omartarek680@gmail.com>
2025-11-16 20:34:16 +0200

    Fix variable name from 'bound_time' to 'time-bound'

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,3 +1,3 @@
 task = input("Enter your task: ")
 priority = input("Priority (high/medium/low) ")
-bound_time = input("Is it time-bound? (yes/no) ")
+time-bound = input("Is it time-bound? (yes/no) ")
