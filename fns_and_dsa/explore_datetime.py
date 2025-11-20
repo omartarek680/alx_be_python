70f068d06993479c8aa056cfbec08841a5bd108d
Omar Tarek <omartarek680@gmail.com>
2025-11-20 15:44:12 +0200

    Explore Date Time

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,16 @@
+import datetime
+def display_current_datetime():
+    current_date = datetime.datetime.now()
+    print("Current date:", current_date.strftime('%Y-%m-%d %H:%M:%S'))
+    return current_date 
+
+def calculate_future_date():
+    current =  display_current_datetime()
+    days = int(input("Enter the number of days to add to the current date: "))
+    future_date = current + datetime.timedelta(days=days)
+    print(f"Future date: {future_date.strftime('%Y-%m-%d')}")
+     
+if __name__ == '__main__':
+      
+    calculate_future_date()
+    
