4bcbe0d22fe0413f294ba999b1527dee7153c860
Omar Tarek <omartarek680@gmail.com>
2025-12-03 23:55:39 +0200

    Bank Account

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,27 @@
+class BankAccount:
+    def __init__(self, account_balance):
+        self.__account_balance = account_balance
+        
+        
+        
+    def deposit(self,amount):
+        self.__account_balance += amount
+        
+        
+    def withdraw(self,amount):
+        if amount > self.__account_balance:
+            return False
+        else :
+            
+            self.__account_balance -= amount 
+            return True
+        
+    
+    def display_balance(self):
+        print(f"Current Balance: ${self.__account_balance}")
+    
+    
+omar = BankAccount(100)
+
+print(omar.display_balance)           
+        
